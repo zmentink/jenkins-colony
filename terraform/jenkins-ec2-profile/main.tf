2249cfbd303a3c5b86fd8d301b321dd937e9222f
Dmytro Dovbii <dmytro.d@quali.com>
2020-09-25 15:12:52 +0300

    Fix TF vars

--- terraform/jenkins-ec2-profile/main.tf
+++ terraform/jenkins-ec2-profile/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "colony_bucket" {
   # bucket = "colony-bucket-${var.SANDBOX_ID}"
-  bucket = "${BUCKET_NAME}"
+  bucket = "${var.BUCKET_NAME}"
   acl    = "private"
   versioning {
     enabled = true
